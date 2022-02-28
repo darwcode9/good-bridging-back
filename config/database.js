4013c363150c824af01e3b2393e6016d9a8c8082
darwcode9 <darwcode9@protonmail.com>
2022-02-28 18:03:22 -0500

    DO deployment setup

---
+++ config/database.js
@@ -0,0 +1,27 @@
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  const config = process.env.DATABASE_URL != undefined ? { ...parse(process.env.DATABASE_URL), ssl: true } : {}
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host || '127.0.0.1',
+          port: config.port || 5432,
+          database: config.database || 'good-bridging-blog',
+          username: config.user || 'postgres',
+          password: config.password || '27137766@Pg',
+          ssl: {
+            rejectUnauthorized: false
+          }
+        },
+        options: {
+          ssl: config.host ? true : false,
+        },
+      },  
+    },
+  }
+};
